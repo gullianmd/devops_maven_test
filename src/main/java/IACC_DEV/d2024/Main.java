f08fd3fdbac99d761144b5280d7f88a2ca8abe4c
Gullian Mardones <g.mardones@outlook.com>
2024-07-29 20:52:30 -0400

    initial commit

---
+++ src/main/java/IACC_DEV/d2024/Main.java
@@ -0,0 +1,19 @@
+package IACC_DEV.d2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+
+		int iteration = 0;
+
+		do {
+			iteration ++;
+			System.out.println(iteration);
+
+		} while (iteration < 10);
+
+	}
+}
