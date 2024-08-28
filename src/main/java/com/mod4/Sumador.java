1c33fc7eec1bb29d7f054b4afd0d833fb3b8f837
Gullian Mardones <g.mardones@outlook.com>
2024-08-27 22:01:10 -0400

    añadido pruebas

--- src/main/java/com/mod4/Sumador.java
+++ src/main/java/com/mod4/Sumador.java
@@ -16,4 +16,25 @@ public class Sumador {
         System.out.println(resultado);
         return res;
     }
+        static int restaValores(int a, int b){
+        
+        int res = a-b;
+        String resultado = String.format("la resta de %d y %d es %d", a, b, res);
+        System.out.println(resultado);
+        return res;
+    }
+    static int divideValores(int a, int b){
+        
+        int res = a/b;
+        String resultado = String.format("la division entre %d y %d es %d", a, b, res);
+        System.out.println(resultado);
+        return res;
+    }
+    static int multiplicaValores(int a, int b){
+        
+        int res = a*b;
+        String resultado = String.format("la multiplicacion entre %d y %d es %d", a, b, res);
+        System.out.println(resultado);
+        return res;
+    }
 }
