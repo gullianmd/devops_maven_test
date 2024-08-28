19bb979ca928bd4c2c11c42276cc8c7c7989dfac
Gullian Mardones <g.mardones@outlook.com>
2024-08-27 21:59:38 -0400

    añadido pruebas

--- src/test/java/com/mod4/AppTest.java
+++ src/test/java/com/mod4/AppTest.java
@@ -12,14 +12,6 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
     
     @Test
     public void testSumaValores() {
@@ -29,6 +21,42 @@ public class AppTest
 
         // Verificar que la salida es la esperada
         int expectedOutput = 10;
+        
+        Assert.assertEquals(res, expectedOutput);
+    }
+    
+    @Test
+    public void testRestaValores() {
+        
+        // Llamar al método que queremos probar
+        int res = Sumador.restaValores(10, 5);
+
+        // Verificar que la salida es la esperada
+        int expectedOutput = 5;
+        
+        Assert.assertEquals(res, expectedOutput);
+    }
+    
+    @Test
+    public void testDivideValores() {
+        
+        // Llamar al método que queremos probar
+        int res = Sumador.divideValores(20, 10);
+
+        // Verificar que la salida es la esperada
+        int expectedOutput = 2;
+        
+        Assert.assertEquals(res, expectedOutput);
+    }  
+    
+    @Test
+    public void testMultiplicaValores() {
+        
+        // Llamar al método que queremos probar
+        int res = Sumador.multiplicaValores(3, 10);
+
+        // Verificar que la salida es la esperada
+        int expectedOutput = 30;
         
         Assert.assertEquals(res, expectedOutput);
     }
